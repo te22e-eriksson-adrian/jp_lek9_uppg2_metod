c7fee80778957ef973c7dac21a6ce70e85176276
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-11-29 10:05:29 +0100

    gjorde klart uppg 2 del A-1 och A-2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //A
+        String svar = capitalize("b");
+        System.out.println("Gemenen 'b' är "+svar+" som versal.");
+        System.out.println("     ");
+
+        //A - 2
+        String svar2 = capitalize("z");
+        System.out.println("Gemenen 'z' är "+svar2+" som versal.");
+        System.out.println("     ");
+    }
+    static String capitalize(String gemen){
+        String versal = gemen.toUpperCase();
+        return versal;
     }
 }
