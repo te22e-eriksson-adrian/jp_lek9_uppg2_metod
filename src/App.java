e4895c84a7ac3d6b60beae57a292fc17f0e055ac
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-12-06 10:02:43 +0100

    finish - gjorde klart uppg2 del D med variationsb.

--- src/App.java
+++ src/App.java
@@ -14,12 +14,19 @@ public class App {
         int tal = 2;
         boolean svar3 = ärPrimtal(tal);
         System.out.println("Är "+tal+" ett primtal?: "+svar3);
+        System.out.println("     ");
 
         //C
         int bas = 2;
         int exponent = 3;
         int svar4 = potens(bas, exponent);
         System.out.println(bas+" upphöjt till "+exponent+" är lika med: "+svar4);
+        System.out.println("     ");
+
+        //D
+        double[] fält = {9,3,5,6,10};
+        int bredd = (int) variationsbredd(fält);
+        System.out.println("Variationsbredden i 'fält' är "+bredd+".");
     }
     static String capitalize(String gemen){
         String versal = gemen.toUpperCase();
@@ -45,5 +52,21 @@ public class App {
             utrakningssvar = utrakningssvar*bas;
         }
         return utrakningssvar;
+    }
+    static double variationsbredd(double falt[]){
+        double störst = falt[0];
+        double minst = falt[falt.length-1];
+        for (int i = 0; i<falt.length; i++){
+            if (störst<falt[i]) {
+                störst = falt[i];
+            }
+        }
+        for (int j = falt.length-1; j>0; j--){
+            if (minst>falt[j]) {
+                minst = falt[j];
+            }
+        }
+        double variation = störst - minst;
+        return variation;
     }
 }
